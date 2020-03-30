8a5a101b158809eee020f0f4e931b52d378f6f3a
Lenovo <get2saravanan@yahoo.co.in>
2020-03-30 11:29:31 +0530

    learn to push to git 2

--- src/test/java/thirtymarchtwentytwenty.java
+++ src/test/java/thirtymarchtwentytwenty.java
@@ -8,5 +8,10 @@ public class thirtymarchtwentytwenty {
     WebDriver driver  = new ChromeDriver();
 
         driver.get("https://personal.natwest.com/personal.html");
+        System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\SeleniumDriver\\chromedriver.exe");
+
+    WebDriver driver  = new ChromeDriver();
+
+        driver.get("https://personal.natwest.com/personal.html");
 }
 
